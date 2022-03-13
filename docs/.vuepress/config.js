f4c7b55bfad9586f81c34dc6157f27339e96534f
JiangHuanLH <2972188187@qq.com>
2022-03-13 09:16:27 +0800

    fix bug 0f icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,13 +15,13 @@ module.exports = {
 		}
     ],
     [
-      "link",
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: `/img/logo/favicon-32x32.png`
-      }
+    	"link",
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: `/img/logo/favicon-32x32.png`
+		}
     ],
     ["link", { rel: "manifest", href: "/manifest.webmanifest" }],
     ["meta", { name: "application-name", content: "JiangHuan's Personal Blog" }],
@@ -36,83 +36,85 @@ module.exports = {
     ],
     ["meta", { name: "theme-color", content: "#377bb5" }],
     ["meta", { name: "msapplication-TileColor", content: "#377bb5" }]
-  ],
-  locales: {
-      "/": {
-        lang: 'zh-cmn-Hans',
-        title: '将焕',
-        description: '将焕的个人博客'
-      }
-  },
-  markdown: {
-      extractHeaders: {
-          level: [2, 3, 4, 5]
-      },
-      code: {
-          lineNumbers: false
-      }
-  },
+	],
+	locales: {
+			"/": {
+				lang: 'zh-cmn-Hans',
+				title: '将焕',
+				description: '将焕的个人博客'
+			}
+	},
+	markdown: {
+		extractHeaders: {
+			level: [2, 3, 4, 5]
+		},
+		code: {
+			lineNumbers: false
+		}
+	},
 
-  bundler:
-  process.env.DOCS_BUNDLER ??
-  "@vuepress/vite",
+	bundler:
+	process.env.DOCS_BUNDLER ??
+	"@vuepress/vite",
 
-  theme: 'gungnir',
-  themeConfig: {
-    repo: 'https://gitee.com/jiang-huan-web/blog.git',
-    docsDir: 'docs',
-    navbar: navbar,
+	theme: 'gungnir',
+	themeConfig: {
+		repo: 'https://gitee.com/jiang-huan-web/blog.git',
+		docsDir: 'docs',
+		navbar: navbar,
 
-    hitokoto: "https://v1.hitokoto.cn?c=d&c=i",
-    
-    personalInfo: {
-        name: '将焕',
-        avatar: "/img/avatar.jpeg",
-        description: 'Web前端爱好者',
-        sns: {
-            github: 'JiangHuanLH',
-            zhihu: '',
-            email: '',
-        }
-    },
-    homeHeaderImages: [
-        {
-            "path": '/img/home-bg/1.jpg',
-            "mask": 'rgb(40, 57, 101, .4)'
-        },
-        {
-            "path": '/img/home-bg/2.jpg',
-            "mask": 'rgb(40, 57, 101, .4)'
-        },
-        {
-            "path": '/img/home-bg/3.jpg',
-            "mask": 'rgb(40, 57, 101, .4)'
-        },
-        {
-            "path": '/img/home-bg/4.jpg',
-            "mask": 'rgb(40, 57, 101, .4)'
-        }
-    ],
-    pages: {
-        tags: {
-            subtitle: '标签页',
-            bgImage: {
-                path: '/img/pages/tags.jpg',
-                mask: 'rbg(211, 136, 37, .5)'
-            }
-        }
-    },
+		hitokoto: "https://v1.hitokoto.cn?c=d&c=i",
+		
+		personalInfo: {
+			name: '将焕',
+			avatar: "/img/avatar.jpeg",
+			description: 'Web前端爱好者',
+			sns: {
+				github: 'JiangHuanLH',
+				zhihu: '',
+				email: '',
+			}
+		},
+		homeHeaderImages: [
+			{
+				"path": '/img/home-bg/1.jpg',
+				"mask": 'rgb(40, 57, 101, .4)'
+			},
+			{
+				"path": '/img/home-bg/2.jpg',
+				"mask": 'rgb(40, 57, 101, .4)'
+			},
+			{
+				"path": '/img/home-bg/3.jpg',
+				"mask": 'rgb(40, 57, 101, .4)'
+			},
+			{
+				"path": '/img/home-bg/4.jpg',
+				"mask": 'rgb(40, 57, 101, .4)'
+			}
+		],
+		pages: {
+			tags: {
+				subtitle: '标签页',
+				bgImage: {
+					path: '/img/pages/tags.jpg',
+					mask: 'rbg(211, 136, 37, .5)'
+				}
+			}
+		},
 
-    search: true,
-    searchMaxSuggestions: 10,
-    searchPlaceholder: '搜索博客',
-    searchIcon: 'ri-archive-drawer-line',
+		search: {
+		searchMaxSuggestions: 10,
+		searchPlaceholder: '搜索',
+		searchIcon: 'ri-archive-drawer-line',
+		},
 
-    footer: `
-        <span>我在某年某月某日醒过来</span>
-        <br />
-        Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
-        <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
-    `
-  }
+
+		footer: `
+			<span>我在某年某月某日醒过来</span>
+			<br />
+			Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
+			<a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
+		`
+	}
 }
